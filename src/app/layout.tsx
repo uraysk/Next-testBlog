8b9f9a06468935d6c25b2bb49f96633f5efca5c0
Ura20 <ura2091.212@gmail.com>
2025-06-28 11:18:06 +0900

    feat:ヘッダーの追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Header } from "./components/organisms/layout/Header";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="container mx-auto bg-slate-800 text-yellow-50">
+        <Header />
         {children}
       </body>
     </html>
